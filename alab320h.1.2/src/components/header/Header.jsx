49e3e8810a4b424e34fe26f9bec0870836d47323
samcoeur <samcoeur2013@gmail.com>
2024-12-05 12:22:23 -0500

    header

---
+++ alab320h.1.2/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+       <h1 style={{color:'tomato'}}>Startre's List</h1>
+       <h2 style={{fontWeight:200}} >Better-Dressed People</h2>
+
+    </div>    
+  )  
+}
+
+export default Header
